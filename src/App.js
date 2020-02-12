5604010f727c943806b7437f0033d423918e425c
keemdb <keemdb@gmail.com>
2020-02-12 22:50:08 +0900

    homework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    userid: "",
+    userpw: ""
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handlePrevent = e => {
+    e.preventDefault();
+  }
+
+  render() {
+    const { userid, userpw } = this.state;
+    return (
+      <div className="homework">
+        <h1>{this.props.title}</h1>
+        <form onSubmit={this.handlePrevent}>
+          <input
+            name="userid"
+            placeholder="ID"
+            value={userid}
+            onChange={this.handleChange}
+          />
+          <input
+            name="userpw"
+            placeholder="Password"
+            type="password"
+            value={userpw}
+            onChange={this.handleChange}
+          />
+          <button 
+            type="submit"
+            disabled={userpw.length < 6}
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
